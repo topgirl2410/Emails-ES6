d6572c363adff2855708b124fd218d63f73cff77
topgirl2410 <maria.topgirl82@gmail.com>
2022-04-28 15:39:27 +0200

    limpiar mensajes de error

--- 02-envio-emails/js/app.js
+++ 02-envio-emails/js/app.js
@@ -3,7 +3,7 @@
 // Variables
 
 const btnEnviar = document.querySelector('#enviar');
-
+const formulario = document.querySelector('#enviar-mail');
 
 // Variables para campos 
 
@@ -22,6 +22,8 @@ function eventListeners() {
     // Campos del formulario 
 
     email.addEventListener('blur', validarFormulario);
+    asunto.addEventListener('blur', validarFormulario);
+    mensaje.addEventListener('blur', validarFormulario);
 }
 
 
@@ -41,5 +43,26 @@ function validarFormulario(e) {
         console.log('si hay algo...');
     } else {
         e.target.classList.add('border', 'border-red-500');
+
+        mostrarError();
     }
+}
+
+
+function mostrarError() {
+    const mensajeError = document.createElement('p');
+    mensajeError.textContent = 'Todos los campos son obligatorios...';
+    mensajeError.classList.add('border', 'border-red-500', 'background-red-100', 'text-red-500', 'p-3',
+        'mt-5', 'text-center', 'error');
+
+
+    const errores = document.querySelectorAll('.error');
+    if (errores.length === 0) {
+
+        //formulario.insertBefore(mensajeError, document.querySelector('.mb-10')); (mostrar en la parte de arriba);
+
+        formulario.appendChild(mensajeError);  // Muestra el error en la parte de abajo
+    }
+
+
 }
