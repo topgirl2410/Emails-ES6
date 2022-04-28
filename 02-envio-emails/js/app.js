318da58124133630bcd00e34316b310b1a1d33da
topgirl2410 <maria.topgirl82@gmail.com>
2022-04-28 22:20:02 +0200

    proyecto acabado

--- 02-envio-emails/js/app.js
+++ 02-envio-emails/js/app.js
@@ -1,77 +1,101 @@
 'use strict';
 
-// Variables
-
-const btnEnviar = document.querySelector('#enviar');
-const formulario = document.querySelector('#enviar-mail');
-
-// Variables para campos 
-
 const email = document.querySelector('#email');
 const asunto = document.querySelector('#asunto');
 const mensaje = document.querySelector('#mensaje');
 
+const btnEnviar = document.querySelector('#enviar');
+const formularioEnviar = document.querySelector('enviar-mail');
+const resetBtn = document.querySelector('#resetBtn');
+
+// EventListeners
+
 eventListeners();
 
 function eventListeners() {
+    // Inicio de la app
+    document.addEventListener('DOMContentLoaded', inicioApp);
 
-    // Cuando la app arranca
-
-    document.addEventListener('DOMContentLoaded', iniciarApp);
-
-    // Campos del formulario 
-
+    // Campos del formulario
     email.addEventListener('blur', validarFormulario);
     asunto.addEventListener('blur', validarFormulario);
     mensaje.addEventListener('blur', validarFormulario);
+
+
+    // Boton de enviar en el submit
+    formularioEnviar.addEventListener('submit', enviarEmail);
+
+    // Boton de reset
+    resetBtn.addEventListener('click', resetFormulario);
 }
 
 
 // Funciones
 
-
-function iniciarApp() {
+function inicioApp() {
+    // Deshabilitar el envio
     btnEnviar.disabled = true;
-    btnEnviar.classList.add('cursor-not-allowed', 'opacity-50');
+    btnEnviar.classList.add('cursor-not-allowed', 'opacity-50')
 }
 
-// Valida el formulario
+// Valida que el campo tenga algo escrito
 
 function validarFormulario(e) {
 
-
-    if (e.target.value.length > 0) {
-        console.log('si hay algo...');
+    if (e.target.value.lenght > 0) {
+        campo.style.borderBottomColor = 'green';
+        campo.classList.remove('error');
     } else {
         e.target.classList.add('border', 'border-red-500');
-
-        mostrarError('Todos los campos son obligatorios');
     }
 
-    if (e.target.type === 'email') {
-        const resultado = e.target.value.indexOf('@');
-        if (resultado < 0) {
-            mostrarError('El email no es valido');
-        }
+    // Validar unicamente el email
+
+    if (this.type === 'email') {
+        validarEmail(this);
+    }
+
+    if (email.value !== '' && asunto.value !== '' && mensaje.value !== '') {
+        btnEnviar.disabled = false;
+        btnEnviar.classList.remove('opacity-50');
+        btnEnviar.classList.remove('cursor-not-allowed');
     }
 }
 
-
-function mostrarError(mensaje) {
-    const mensajeError = document.createElement('p');
-    mensajeError.textContent = mensaje;
-    mensajeError.classList.add('border', 'border-red-500', 'background-red-100', 'text-red-500', 'p-3',
-        'mt-5', 'text-center', 'error');
-
-
-    const errores = document.querySelectorAll('.error');
-    if (errores.length === 0) {
-
-        //formulario.insertBefore(mensajeError, document.querySelector('.mb-10')); (mostrar en la parte de arriba);
-
-        formulario.appendChild(mensajeError);  // Muestra el error en la parte de abajo
-    }
-
-
+// Resetear el formulario
+function resetFormulario(e) {
+    formularioEnviar.reset();
+    e.preventDefault();
 }
 
+
+// Cuando se envia el correo
+
+function enviarEmail(e) {
+
+    e.preventDefault();
+
+    // Spinner al presionar enviar 
+
+    const spinner = document.querySelector('#spinner');
+    spinner.style.display = 'flex';
+
+    // Gif que envia email
+
+    const enviado = document.createElement('p');
+    enviado.textContent = 'Mensaje Enviado Correctamente';
+    enviado.classList.add('bg');
+
+
+    // Ocultar Spinner y mostrar gif de enviado 
+    setTimeout(() => {
+        spinner.style.display = 'none';
+
+        document.querySelector('#loaders').appendChild(enviado);
+
+        setTimeout(() => {
+            enviado.remove();
+            formularioEnviar.reset();
+        }, 5000)
+    }, 3000)
+}
