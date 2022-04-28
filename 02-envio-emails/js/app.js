05b8ff4f2cb29df1d9e5b18165496cd377e10658
topgirl2410 <maria.topgirl82@gmail.com>
2022-04-28 16:40:49 +0200

    validar un email

--- 02-envio-emails/js/app.js
+++ 02-envio-emails/js/app.js
@@ -39,19 +39,27 @@ function iniciarApp() {
 
 function validarFormulario(e) {
 
+
     if (e.target.value.length > 0) {
         console.log('si hay algo...');
     } else {
         e.target.classList.add('border', 'border-red-500');
 
-        mostrarError();
+        mostrarError('Todos los campos son obligatorios');
+    }
+
+    if (e.target.type === 'email') {
+        const resultado = e.target.value.indexOf('@');
+        if (resultado < 0) {
+            mostrarError('El email no es valido');
+        }
     }
 }
 
 
-function mostrarError() {
+function mostrarError(mensaje) {
     const mensajeError = document.createElement('p');
-    mensajeError.textContent = 'Todos los campos son obligatorios...';
+    mensajeError.textContent = mensaje;
     mensajeError.classList.add('border', 'border-red-500', 'background-red-100', 'text-red-500', 'p-3',
         'mt-5', 'text-center', 'error');
 
@@ -66,3 +74,4 @@ function mostrarError() {
 
 
 }
+
